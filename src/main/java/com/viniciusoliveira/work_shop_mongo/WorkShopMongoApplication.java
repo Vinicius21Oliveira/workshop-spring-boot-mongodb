599c6608db95985f2a2ee767ca8a2c7e98785aa4
Vinicius21Oliveira <viniciusoliveiraa8004@gmail.com>
2024-04-24 05:43:47 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/viniciusoliveira/work_shop_mongo/WorkShopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusoliveira.work_shop_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusoliveira/work_shop_mongo/WorkShopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusoliveira.work_shop_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
