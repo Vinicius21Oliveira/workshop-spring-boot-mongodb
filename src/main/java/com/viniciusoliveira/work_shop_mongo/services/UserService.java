ed35017600266e0f023f441e5439a355fcad79d8
Vinicius21Oliveira <viniciusoliveiraa8004@gmail.com>
2024-04-26 20:51:40 -0300

    Inserção de usuário com POST

--- src/main/java/com/viniciusoliveira/work_shop_mongo/services/UserService.java
+++ src/main/java/com/viniciusoliveira/work_shop_mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.viniciusoliveira.work_shop_mongo.domain.User;
+import com.viniciusoliveira.work_shop_mongo.dto.UserDTO;
 import com.viniciusoliveira.work_shop_mongo.repository.UserRepository;
 import com.viniciusoliveira.work_shop_mongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
