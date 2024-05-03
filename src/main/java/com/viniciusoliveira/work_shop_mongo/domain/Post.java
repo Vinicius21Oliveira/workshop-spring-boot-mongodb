3427a4a5c5d2253782f303ea16400aa5d8ee4923
Vinicius21Oliveira <viniciusoliveiraa8004@gmail.com>
2024-05-02 21:46:18 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/viniciusoliveira/work_shop_mongo/domain/Post.java
+++ src/main/java/com/viniciusoliveira/work_shop_mongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.viniciusoliveira.work_shop_mongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.viniciusoliveira.work_shop_mongo.dto.AuthorDTO;
+import com.viniciusoliveira.work_shop_mongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -32,6 +35,9 @@ public class Post implements Serializable {
 		this.body = body;
 		this.author = author;
 	}
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 
 	public String getId() {
 		return id;
@@ -72,6 +78,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -89,9 +103,5 @@ public class Post implements Serializable {
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
-	
-	
 	
 }
